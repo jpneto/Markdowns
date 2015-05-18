bb9db6f7baddf78d27a281b861cee84293de3859
jpneto <jpn@di.fc.ul.pt>
2015-05-18 12:02:24 +0100

    maps for Portugal+Bishop's  PRML chapter 3
    maps for Portugal includes functions to map district and municipe data
    on Portugal's map
    
    Added source code to complement Bishop's Chapter 3 of his PRML book

---
+++ PRML/chp3/js/bayes_reg.js
@@ -0,0 +1,12 @@
+(function($) {
+    $(document).ready(function() {
+	
+	$('#bayes_reg').scianimator({
+	    'images': ['images/bayes_reg1.png', 'images/bayes_reg2.png', 'images/bayes_reg3.png', 'images/bayes_reg4.png', 'images/bayes_reg5.png', 'images/bayes_reg6.png', 'images/bayes_reg7.png', 'images/bayes_reg8.png', 'images/bayes_reg9.png', 'images/bayes_reg10.png', 'images/bayes_reg11.png', 'images/bayes_reg12.png', 'images/bayes_reg13.png', 'images/bayes_reg14.png', 'images/bayes_reg15.png', 'images/bayes_reg16.png', 'images/bayes_reg17.png', 'images/bayes_reg18.png', 'images/bayes_reg19.png', 'images/bayes_reg20.png', 'images/bayes_reg21.png', 'images/bayes_reg22.png', 'images/bayes_reg23.png', 'images/bayes_reg24.png', 'images/bayes_reg25.png', 'images/bayes_reg26.png', 'images/bayes_reg27.png', 'images/bayes_reg28.png', 'images/bayes_reg29.png', 'images/bayes_reg30.png', 'images/bayes_reg31.png'],
+	    'width': 800,
+	    'delay': 1000,
+	    'loopMode': 'loop'
+	});
+	$('#bayes_reg').scianimator('play');
+    });
+})(jQuery);
